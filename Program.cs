7674df47e038fe3cb4c7aab24f43be4b76cdb708
Anduin2017 <Anduin@aiursoft.com>
2018-05-07 03:01:08 +0800

    Migrate project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Aiursoft.Pylon;
+using Aiursoft.Wiki.Data;
+
+namespace Aiursoft.Wiki
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args)
+                .MigrateDbContext<WikiDbContext>()
+                .Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                 .UseStartup<Startup>()
+                 .Build();
+
+            return host;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Wiki On Containers
